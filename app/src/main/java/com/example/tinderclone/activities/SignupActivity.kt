2baf31403e718757a4455ed4dc6748a69afd7fc1
user_101 <uluk1507@gmail.com>
2020-08-11 00:15:31 -0500

    user creation

--- app/src/main/java/com/example/tinderclone/activities/SignupActivity.kt
+++ app/src/main/java/com/example/tinderclone/activities/SignupActivity.kt
@@ -5,16 +5,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.tinderclone.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.android.synthetic.main.activity_login.passwordET as passwordET1
 
 class SignupActivity : AppCompatActivity() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()//ref to the firebaseAuth service
+    private val firebaseAuthListener =
+        FirebaseAuth.AuthStateListener {
+            //check if the user exists
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                startActivity(MainActivity.newIntent(this))
+                finish()
+            }
+        }///this is basically called when the user created and authenticated
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
 
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
     fun onSignup(v: View) {
-    startActivity(MainActivity.newIntent(this))
+        if (!emailET.text.toString().isNullOrEmpty() && !passwordET.text.toString().isNullOrEmpty()) {
+            //instruction to create the user
+            //if this task completes successfully then startActivity takes you to the MainActivity
+            firebaseAuth.createUserWithEmailAndPassword(
+                emailET.text.toString(),
+                passwordET.text.toString()
+            ).addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Toast.makeText(this, "Signup error ${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     companion object {
